fa0d2cceb152c0bba48b57d0126e831f61d80ecc
lvbr <lv.barbo@gmail.com>
2016-04-16 14:14:39 -0700

    Create dplyr

---
+++ notes.R
@@ -0,0 +1,5 @@
+#################
+#Intro to Rmarkdown and github
+##################
+
+# .gitignore : ignore the files that I don't want to pay attention to them, Every data or document that I want to ignore I should tell .gitignore to ignore them. If it is possible start by telling bash wich documents you want to ignore 
