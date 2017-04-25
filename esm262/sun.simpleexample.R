3364e1278fef4ad1ace7361d7928be61080a2cc3
lvbr <lv.barbo@gmail.com>
2017-04-24 18:11:49 -0700

    changed color

--- esm262/sun.simpleexample.R
+++ esm262/sun.simpleexample.R
@@ -3,6 +3,6 @@ sunlight = read.table("esm262/sun.txt", header=T)
 par(mar=c(5,6,3,2))
 boxplot(sunlight$Kdown_direct~sunlight$month,
         ylab="Downwelling Solar\n kj/m2/day",
-        xlab="month", col="orange",
+        xlab="month", col="red",
         main="Idk what is this")
 
