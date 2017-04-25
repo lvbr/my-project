4922708db470011fe45356da1b0a6e907c3352d0
lvbr <lv.barbo@gmail.com>
2017-04-24 18:18:31 -0700

    getting it again

--- esm262/sun.simpleexample.R
+++ esm262/sun.simpleexample.R
@@ -3,6 +3,6 @@ sunlight = read.table("esm262/sun.txt", header=T)
 par(mar=c(5,6,3,2))
 boxplot(sunlight$Kdown_direct~sunlight$month,
         ylab="Downwelling Solar\n kj/m2/day",
-        xlab="month", col="yellow",
+        xlab="month", col="green",
         main="Idk what is this")
 
